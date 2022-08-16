5542c5d8c53d85699728417765d3ae0dce7fad0b
Aly maamoun <aly.mammoun18@gmail.com>
2022-08-16 04:12:35 +0200

    updates

---
+++ EXE/PORT_CFG.h
@@ -0,0 +1,11 @@
+/**********************************************************************************************************
+*
+*	File name: 		PORT_def.h
+*	Author: 			Aly Mohamed Maamoun
+*	Date:		  		15/7/2022
+*	Description:	Header file for port module	
+***********************************************************************************************************/
+#ifndef PORT_CFG_h
+#define PORT_CFG_h
+#define PORT_CONFIGURED_CHANNELS 4
+#endif

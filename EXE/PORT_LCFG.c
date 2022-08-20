a2874e654da72b209390ab849134c60fe2a98530
Aly Mammoun <53837104+alymaamoun@users.noreply.github.com>
2022-08-20 19:06:00 +0200

    Delete EXE directory

--- EXE/PORT_LCFG.c
+++
@@ -1,6 +0,0 @@
-#include "PORT.h"
-const Port_ConfigType Port_config[PORT_CONFIGURED_CHANNELS] ={
-PORT_def_PORTA,PORT_def_Pin5,OUTPUT,Port_def_Mode_DIO,OFF,_4MA,
-PORT_def_PORTA,PORT_def_Pin7,INPUT,Port_def_Mode_M1PWM,PULL_UP,_8MA,
-PORT_def_PORTD,PORT_def_Pin0,INPUT,Port_def_Mode_UART,OFF,_2MA,
-};
